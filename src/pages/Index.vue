a16dfec9f3e12fdd659b3f4b07f41ae839baba77
cybanjar <cybanjar@gmail.com>
2020-12-12 20:50:15 +0700

    modal maximized

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -15,7 +15,7 @@
           :key="surah.id"
           class="col-4 col-xs-12 col-sm-3 col-md-3 q-ma-md"
         >
-          <q-card @click="goToAyah(surah)" flat bordered class="q-mx-sm">
+          <q-card @click="dialog = true" flat bordered class="q-mx-sm">
             <q-card-section>
               <div class="text-h6">
                 <span class="number">{{ surah.number }}</span>
@@ -30,24 +30,75 @@
           </q-card>
 
           <q-btn
-            class="q-mt-xl"
-            color="white"
-            text-color="blue"
-            unelevated
-            to="/landing"
-            label="Go Home"
-            no-caps
+            flat
+            class="bg-primary text-white"
+            @click="goToAyah(surahs)"
+            label="Save"
           />
         </div>
       </div>
     </section>
+    <q-dialog
+      v-model="dialog"
+      persistent
+      :maximized="maximizedToggle"
+      transition-show="slide-up"
+      transition-hide="slide-down"
+    >
+      <q-card class="bg-primary text-white">
+        <q-bar>
+          <q-space />
+
+          <q-btn
+            dense
+            flat
+            icon="minimize"
+            @click="maximizedToggle = false"
+            :disable="!maximizedToggle"
+          >
+            <q-tooltip
+              v-if="maximizedToggle"
+              content-class="bg-white text-primary"
+              >Minimize</q-tooltip
+            >
+          </q-btn>
+          <q-btn
+            dense
+            flat
+            icon="crop_square"
+            @click="maximizedToggle = true"
+            :disable="maximizedToggle"
+          >
+            <q-tooltip
+              v-if="!maximizedToggle"
+              content-class="bg-white text-primary"
+              >Maximize</q-tooltip
+            >
+          </q-btn>
+          <q-btn dense flat icon="close" v-close-popup>
+            <q-tooltip content-class="bg-white text-primary">Close</q-tooltip>
+          </q-btn>
+        </q-bar>
+
+        <q-card-section>
+          <div class="text-h6">Alert {{ surahs[0]["englishName"] }}</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+          repellendus sit voluptate voluptas eveniet porro. Rerum blanditiis
+          perferendis totam, ea at omnis vel numquam exercitationem aut, natus
+          minima, porro labore.
+        </q-card-section>
+      </q-card>
+    </q-dialog>
   </q-page>
 </template>
 
 <script>
 import axios from "axios";
 import router from "../router";
-import Vue from 'vue';
+import Vue from "vue";
 
 export default {
   name: "customer",
@@ -62,6 +113,8 @@ export default {
       loading: false,
       showModal: false,
       data: null,
+      dialog: false,
+      maximizedToggle: true,
     };
   },
 
@@ -94,9 +147,9 @@ export default {
   methods: {
     goToAyah(row) {
       //this.showModal = true;
-      // console.log(row);
+      console.log("row", row);
       // router.push({ name: '/landing' })
-      alert(row["name"]);
+      // alert(row["name"]);
     },
   },
 };
